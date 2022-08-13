e5993b6776b9e18c9afe5235e78dd494554ddde5
Philippe OURSEL <philippe@remazing.eu>
2022-08-13 19:28:31 +0200

    create first draft for sheep body & head design

--- src/components/JumpingSheeps.vue
+++ src/components/JumpingSheeps.vue
@@ -2,10 +2,42 @@
   <div class="c-jsheeps">
 
     <div class="sheep">
+
       <div class="sheep-head">
-        <div class="sheep-eyes" />
-        <div class="sheep-eyes" />
+        <div class="sheep-head-wool" />
+        <div class="sheep-head-wool" />
+        <div class="sheep-head-wool" />
+
+        <div class="sheep-head-ears">
+          <div class="sheep-head-ears-ear"/>
+          <div class="sheep-head-ears-ear" />
+        </div>
+
+        <div class="sheep-head-eyes">
+          <div class="sheep-head-eyes-eye">
+            <div class="sheep-head-eyes-eye-pupil">
+            </div>
+          </div>
+
+          <div class="sheep-head-eyes-eye">
+            <div class="sheep-head-eyes-eye-pupil">
+            </div>
+          </div>
+        </div>
       </div>
+
+      <div class="sheep-body">
+        <div class="sheep-body-wools">
+          <div class="sheep-body-wools-wool" />
+          <div class="sheep-body-wools-wool" />
+          <div class="sheep-body-wools-wool" />
+          <div class="sheep-body-wools-wool" />
+          <div class="sheep-body-wools-wool" />
+          <div class="sheep-body-wools-wool" />
+        </div>
+      </div>
+
+
     </div>
 
 
